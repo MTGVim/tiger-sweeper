b7df3871bf573649ef85a9a894a7a0b32623b874
MTGVim <takun617@gmail.com>
2026-02-14 01:05:22 +0900

    chore: switch favicon and pwa icons to png

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icons/icon.svg'],
+      includeAssets: ['favicon.png', 'icons/icon-192.png', 'icons/icon-512.png'],
       manifest: {
         name: 'Tiger-Sweeper',
         short_name: 'TigerSweeper',
@@ -21,14 +21,14 @@ export default defineConfig({
         theme_color: '#111111',
         icons: [
           {
-            src: 'icons/icon.svg',
+            src: 'icons/icon-192.png',
             sizes: '192x192',
-            type: 'image/svg+xml'
+            type: 'image/png'
           },
           {
-            src: 'icons/icon.svg',
+            src: 'icons/icon-512.png',
             sizes: '512x512',
-            type: 'image/svg+xml'
+            type: 'image/png'
           }
         ]
       },
